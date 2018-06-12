398ceabb3490ff08bc00de770ebd32a77b82c409
Fabien Grondin <0fabien0@gmail.com>
2018-06-12 17:47:32 +0200

    Ajout active element pour le menu de navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="/js/menu.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -39,9 +40,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                       <li><a href="{{ route('event.index') }}" class="nav-link">Événements prévus</a></li>
+                      <li><a href="{{ route('event.create') }}" class="nav-link">Créer un nouvel événement</a></li>
                       {{-- <li><a href="{{ route('search.date') }}" class="nav-link">Chercher un événement</a></li> --}}
                       @auth
-                        <li><a href="{{ route('event.create') }}" class="nav-link">Créer un nouvel événement</a></li>
                         <li><a href="{{ route('user.events') }}" class="nav-link">Mes événements</a></li>
                       @endauth
 
