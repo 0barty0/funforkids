e1a451d2c0984a1b8938ca12eeda91d108953c3b
Fabien Grondin <0fabien0@gmail.com>
2018-06-13 20:18:48 +0200

    Fichier externe js du formulaire

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -91,45 +91,6 @@
 @endsection
 
 @section('scripts')
-  <script>
-  var autocomplete;
-  function initAutocomplete() {
-      // Create the autocomplete object, restricting the search to geographical
-      // location types.
-      let input = document.getElementById('place'),
-          options = {types: [], placeIdOnly: true, componentRestrictions: {country: 'fr'}};
-
-      autocomplete = new google.maps.places.Autocomplete(input,options);
-
-      autocomplete.addListener('place_changed', getPlaceId);
-    }
-
-    function getPlaceId() {
-      let place = autocomplete.getPlace();
-      $('input[name=place_id]').val(place['place_id']);
-      $('input[name=place_verification]').val(place['name']);
-    }
-  </script>
+  <script src="/js/form.js"></script>
   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBjExSHAuBYPmeKLtZAoVtnPRt43yA6bpw&libraries=places&callback=initAutocomplete" async defer></script>
-  <script>
-    $(function() {
-      $('.custom-file-input').on('change', function() {
-        let filename = document.getElementById('file-input').files[0].name;
-        $(this).next('.custom-file-label').html(filename);
-      });
-
-      // Form validation
-      $('form').on('submit', function(e){
-        // Verify if a google place has been selected
-        let place = $('input[name=place]').val(),
-            place_verification = $('input[name=place_verification]').val();
-        if (place !== place_verification) {
-          e.preventDefault();
-          e.stopPropagation();
-          $('input[name=place]').addClass('is-invalid');
-          $('input[name=place]').parent().append('<div class="invalid-feedback">Veuillez entrer une adresse valide</div>')
-        }
-      });
-    });
-  </script>
 @endsection
